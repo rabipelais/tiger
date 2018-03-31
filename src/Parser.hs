58f4b491e3907b69cb595373fc1535dac935553d
Sebastian Mendez <sebas.chinoir@gmail.com>
2018-03-31 03:18:29 +0200

    Add Parser (not working).

---
+++ src/Parser.hs
@@ -0,0 +1,324 @@
+module Parser where
+
+import           Protolude                  hiding (many, option, try, (<|>))
+
+import           Text.Megaparsec
+import           Text.Megaparsec.Language   (javaStyle)
+
+import qualified Text.Megaparsec.Char.Lexer as Tok
+import qualified Text.Megaparsec.Expr       as Ex
+
+import           AbSyn
+import           Symbol                     hiding (symbol)
+
+lexer :: Tok.TokenParser ()
+lexer = Tok.makeTokenParser style
+  where
+    ops = ["+", "*", "-", "/", "<", "<=", ">", ">=", ":=", "&", "|"]
+    names = ["array", "break", "do", "else", "end", "for", "function", "if", "in", "let",
+             "nil", "of", "then", "to", "type", "var", "while"]
+    style = javaStyle {
+      Tok.reservedOpNames = ops
+      , Tok.reservedNames = names}
+
+integer :: Parser Integer
+integer = Tok.integer lexer
+
+parens :: Parser a -> Parser a
+parens = Tok.parens lexer
+
+braces :: Parser a -> Parser a
+braces = Tok.braces lexer
+
+brackets :: Parser a -> Parser a
+brackets = Tok.brackets lexer
+
+commaSep :: Parser a -> Parser [a]
+commaSep = Tok.commaSep lexer
+
+semiSep :: Parser a -> Parser [a]
+semiSep = Tok.semiSep lexer
+
+identifier :: Parser Text
+identifier = toS <$> Tok.identifier lexer
+
+reserved :: Text -> Parser ()
+reserved = Tok.reserved lexer . toS
+
+symbol :: Text -> Parser Text
+symbol t = toS <$> (Tok.symbol lexer $ toS t)
+
+reservedOp :: Text -> Parser ()
+reservedOp = Tok.reservedOp lexer . toS
+
+stringLiteral :: Parser Text
+stringLiteral = toS <$> Tok.stringLiteral lexer
+
+whiteSpace :: Parser ()
+whiteSpace = Tok.whiteSpace lexer
+
+------------------------------------------------------
+-- Parser -------------------------------------------
+
+binary s f assoc = Ex.Infix (reservedOp s >> return (\l r -> OpExp l f r)) assoc
+prefix s f = Ex.Prefix (reservedOp s >> return (\x -> OpExp (IntExp 0) f x))
+
+table = [
+      [prefix "-" MinusOp],
+
+      [binary "*" TimesOp Ex.AssocLeft,
+      binary "/" DivideOp Ex.AssocLeft],
+
+      [binary "+" PlusOp Ex.AssocLeft,
+      binary "-" MinusOp Ex.AssocLeft],
+
+      [binary "=" EqOp Ex.AssocNone,
+      binary "<>" NeqOp Ex.AssocNone,
+      binary "<=" LeOp Ex.AssocNone,
+      binary "<" LtOp Ex.AssocNone,
+      binary ">=" GeOp Ex.AssocNone,
+      binary ">" GtOp Ex.AssocNone],
+
+      [binary "&" AndOp Ex.AssocRight],
+      [binary "|" OrOp Ex.AssocRight]]
+
+newSymbol i = Symbol i 0
+-------------------------------------------------------------------------------
+------------------------- Declarations Parsers --------------------------------
+
+-- | Parses the type declarations, following this grammar:
+--
+-- @
+--tydec    -> /type/ type-id = ty
+--
+--ty       -> type-id
+--         -> { tyfields }     (here the braces stand for themselves)
+--         -> /array of/ type-id
+--
+--tyfields -> id : type-id {, id : type-id}
+-- @
+typeDec :: Parser Dec
+typeDec = do
+  reserved "type"
+  typeid <- identifier
+  symbol "="
+  (TypeDec (newSymbol typeid)) <$> (try typeArray <|> typeRecord <|>aType)
+  where
+    typeArray = ArrayTy <$> (reserved "array of" >> identifier >>= return . newSymbol)
+    typeRecord = do
+      records <- braces $ commaSep record
+      return $ RecordTy records
+    aType = NameTy . newSymbol <$>  identifier
+
+record :: Parser Field
+record = do
+  name <- newSymbol <$> identifier
+  symbol ":"
+  typename <- newSymbol <$> identifier
+  return $ Field name typename
+
+-- | Parses a variable declaration:
+--
+-- @
+--vardec -> /var/ id /:=/ exp
+--       -> /var/ id : type-id /:=/ exp
+-- @
+varDec :: Parser Dec
+varDec = do
+  symbol "var"
+  name <- newSymbol <$> identifier
+  typeMaybe <- option Nothing (try (do {symbol ":"; x <- newSymbol <$> identifier; return $ Just x}))
+  symbol ":="
+  exp <- expr
+  return $ VarDec name typeMaybe exp
+
+-- | Parses a procedure (that which returns void) or a function
+--
+-- @
+--fundec -> /function/ id ( tyfields ) /=/ exp
+--       -> /function/ id ( tyfields ) : type-id /=/ exp
+-- @
+funDec :: Parser Dec
+funDec = FunDec <$> function
+
+function :: Parser Function
+function = do
+  reserved "function"
+  name <- newSymbol <$> identifier
+  fields <- parens $ commaSep record
+  typeMaybe <- option Nothing (try (do {symbol ":"; x <- newSymbol <$> identifier; return $ Just x}))
+  symbol "="
+  exp <- try expr <|> parseSeq
+  return $ Function name fields typeMaybe exp
+
+dec :: Parser Dec
+dec = choice [typeDec, varDec, funDec]
+
+------------------------------------------------------------------------------
+-------------------------- Expressions Parsers --------------------------------
+
+-- | Parse @l-values@, which can be normal variables, field accessors,
+-- or array subscripts:
+--
+-- @
+--lvalue -> id
+--       -> lvalue . id
+--       -> lvalue [ expr ]
+-- @
+-- This grammar is kind of confusing and left recursive, we use the
+-- combinator `lfact` as a helper transformation.
+
+var :: Parser Exp
+var = VarExp <$> lfact pId (fieldVar <|> subVar)
+  where
+    fieldVar = do
+      symbol "."
+      name <- newSymbol <$> identifier
+      return (\x -> FieldVar x name)
+    subVar = do
+      exp <- brackets $ try expr <|> simpleExpr
+      return (\x -> SubscriptVar x exp)
+    pId = SimpleVar . newSymbol <$> identifier
+
+-- | This combinator does ab* in a left-associative way.
+-- Applicable when you have a CFG rule with left recursion
+-- which you might rewrite into EBNF X -> YZ*.
+lfact :: Parser a -> Parser (a -> a) -> Parser a
+lfact p q = do { a <- p
+               ; fs <- many q
+               ; return (foldl  (\x f -> f x) a fs)
+               }
+
+-- | Parse the reserved word __nil__.
+nil :: Parser Exp
+nil = reserved "nil" >> return NilExp
+
+-- | Parse a sequence of decimal digits as an /int/.
+int :: Parser Exp
+int = IntExp <$> integer
+
+-- | Parser strings are delimited by quotes (\").
+string :: Parser Exp
+string = StringExp <$> stringLiteral
+
+binOp = Ex.makeExpressionParser simpleExpr table
+
+-- | Parses function call of the form @id (args)@
+funCall :: Parser Exp
+funCall = do
+  name <- newSymbol <$> identifier
+  args <- parens (commaSep expr)
+  return $ CallExp name args
+
+-- | Parses pyte record creation of the form @typeid {id=expr{, id=expr}}@.
+recordExp :: Parser Exp
+recordExp = do
+  typeid <- newSymbol <$> identifier
+  records <- braces $ commaSep record
+  return $ RecordExp records typeid
+  where
+    record = do
+      name <- newSymbol <$> identifier
+      symbol "="
+      exp <- expr
+      return (name, exp)
+
+-- | A sequence of expressions @{expr{; expr}}@
+parseSeq = SeqExp <$> (parens exprs <|> exprs)
+  where
+    exprs = sepBy1 expr (symbol ";")
+
+-- | An assignment expression @lvar := expr@
+assign :: Parser Exp
+assign = do
+  (VarExp lvar) <- var
+  symbol ":="
+  exp <- expr
+  return $ AssignExp lvar exp
+
+-- | Parses an if statement of the form @if exp1 then exp2 [else exp3]@ where
+-- the @else@ part is optional.
+parseIf :: Parser Exp
+parseIf = do
+  reserved "if"
+  cond <- expr
+  reserved "then"
+  res <- expr
+  alt <- option Nothing $ Just <$> (reserved "else" >> expr)
+  return $ IfExp cond res alt
+
+-- | @/while/ expr1 /do/ expr2@
+while :: Parser Exp
+while = do
+  reserved "while"
+  cond <- expr
+  reserved "do"
+  body <- expr
+  return $ WhileExp cond body
+
+-- | @/for/ id := expr1 /to/ expr2 /do/ expr3@.
+for :: Parser Exp
+for = do
+  reserved "for"
+  name <- newSymbol <$> identifier
+  reserved ":="
+  lo <- expr
+  reserved "to"
+  hi <- expr
+  reserved "do"
+  body <- expr
+  return $ ForExp name lo hi body
+
+-- | @/let/ decs /in/ expseq /end/@.
+parseLet :: Parser Exp
+parseLet = do
+  reserved "let"
+  decs <- many dec
+  reserved "in"
+  exp <- parseSeq
+  reserved "end"
+  return $ LetExp decs exp
+
+-- | @break@ statement.
+break :: Parser Exp
+break = reserved "break" >> return BreakExp
+
+-- | An array @typeid [exp1] of exp2@, where @exp1@ is the size and
+-- @exp2@ the initial values in the array
+array :: Parser Exp
+array = do
+  typeid <- newSymbol <$> identifier
+  size <- brackets expr
+  reserved "of"
+  initVal <- expr
+  return $ ArrayExp typeid size initVal
+
+simpleExpr :: Parser Exp
+simpleExpr = choice
+  [ parens binOp
+  , nil
+  , int
+  , Parser.string
+  , parens expr
+  , try funCall
+  , try recordExp
+  , array
+  , assign
+  , var]
+
+expr :: Parser Exp
+expr = choice
+  [ Parser.break
+  , parseIf
+  , while
+  , Parser.for
+  , parseLet
+  , binOp
+  , parseSeq]
+
+-- | A valid Tiger program is an expression, possibly with whitespace before.
+parseProgram :: Parser Exp
+parseProgram = whiteSpace >> expr
+
+parser :: Text -> Text -> Either ParseError Exp
+parser name input = parse parseProgram (toS name) (toS input)
