e63a21474c3f81e991623530f86eaa4474e5b236
Sebastian Mendez <sebas.chinoir@gmail.com>
2018-11-29 03:09:29 +0100

    Test single function to environment and typecheck.

--- test/SemantSpec.hs
+++ test/SemantSpec.hs
@@ -35,43 +35,49 @@ shouldErrorWith a e = (evalTrans a) `shouldBe` (Left e)
 spec :: Spec
 spec = do
   describe "Translate declarations" $ do
+    let e = emptyStringTable
     it "inserts var type on declaration" $ do
-      let e = emptyStringTable
-          (var, t) = symbol "var" e
+      let (var, t) = symbol "var" e
           (ty, t') = symbol "ty" t
           res = Envs (enter var (VarEntry Types.Int) baseVEnv) baseTEnv
       transDec (VarDec var Nothing (IntExp 10)) `shouldEvalTo` (Right res)
     it "errors if type constraint doesn't match var expression" $ do
-      let e = emptyStringTable
-          (var, t) = symbol "var" e
+      let (var, t) = symbol "var" e
           (ty, t') = symbol "ty" t
       transDec (VarDec var (Just ty) (IntExp 10)) `shouldErrorWith` (MismatchedType "")
     it "enters simple type into type environment" $ do
-      let e = emptyStringTable
-          (var, t) = symbol "tyvar" e
+      let (var, t) = symbol "tyvar" e
           (ty, t') = symbol "int" t
           res = Envs baseVEnv (enter var Types.Int baseTEnv)
       transDec (TypeDec var (NameTy ty)) `shouldEvalTo` (Right res)
     it "enters simple type from another type" $ do
-      let e = emptyStringTable
-          (var, t) = symbol "tyvar" e
+      let (var, t) = symbol "tyvar" e
           (ty, t') = symbol "int" t
           (var', t'') = symbol "tyvarr" t'
           res = Envs baseVEnv (enter var' Types.Int $ enter var Types.Int baseTEnv)
       transDecs [TypeDec var (NameTy ty), TypeDec var' (NameTy var)] `shouldEvalTo` (Right res)
     it "enters array type into type environment" $ do
-      let e = emptyStringTable
-          (var, t) = symbol "tyvar" e
+      let (var, t) = symbol "tyvar" e
           (ty, t') = symbol "int" t
           res = Envs baseVEnv (enter var (Types.Array Types.Int (Unique 0)) baseTEnv)
       transDec (TypeDec var (ArrayTy ty)) `shouldEvalTo` (Right res)
     it "enters variable of array type into environment" $ do
-      let e = emptyStringTable
-          (var, t) = symbol "tyvar" e
+      let (var, t) = symbol "tyvar" e
           (ty, t') = symbol "int" t
           (var', t'') = symbol "arrayvar" t'
           res = Envs (enter var' (VarEntry (Types.Array Types.Int (Unique 0))) baseVEnv) (enter var (Types.Array Types.Int (Unique 0)) baseTEnv)
       transDecs [TypeDec var (ArrayTy ty), VarDec var' (Just var) (ArrayExp var (IntExp 10) (IntExp 0))] `shouldEvalTo` (Right res)
+    it "enters function into environment" $ do
+      let (fun, t) = symbol "fun" e
+          res = Envs (enter fun (FunEntry [] Types.Int) baseVEnv) baseTEnv
+      transDec (FunDec (Function fun [] Nothing (IntExp 10))) `shouldEvalTo` (Right res)
+    it "typechecks function body and type" $ do
+      let (fun, t) = symbol "fun" e
+          res = Envs (enter fun (FunEntry [] Types.Int) baseVEnv) baseTEnv
+      transDec (FunDec (Function fun [] (Just (Symbol "int")) (IntExp 10))) `shouldEvalTo` (Right res)
+    it "errors if function body and type don't match" $ do
+      let (fun, t) = symbol "fun" e
+      transDec (FunDec (Function fun [] (Just (Symbol "string")) (IntExp 10))) `shouldErrorWith` (MismatchedType "")
 
   describe "Tranlate expressions" $ do
     describe "Op arithmetic" $ do
